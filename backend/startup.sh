976ecce0bb5983df4066d7b90f1050f054aea04c
Lucas Dousseau Arantes <53654475+xoLucas@users.noreply.github.com>
2026-05-31 22:17:34 -0300

    Add startup script with migrations and seed

---
+++ backend/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python manage.py migrate --settings=marcenaria.settings
+python manage.py seed_dados --settings=marcenaria.settings
+gunicorn marcenaria.wsgi:application --bind 0.0.0.0:$PORT
